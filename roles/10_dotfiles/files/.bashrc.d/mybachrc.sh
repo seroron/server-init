6bd7d4902c2a06f10d80ff9dbdddd73b36a43507
Kenichi HIROSE <seroron@outlook.com>
2016-10-19 11:17:21 +0000

    change PS1 style

--- roles/10_dotfiles/files/.bashrc.d/mybachrc.sh
+++ roles/10_dotfiles/files/.bashrc.d/mybachrc.sh
@@ -1,6 +1,9 @@
 # undef ctrl+s
 stty stop undef
 
+# append to the history file, don't overwrite it
+shopt -s histappend
+
 # bash history size
 HISTSIZE=10000
 HISTFILESIZE=10000
@@ -8,3 +11,10 @@ HISTFILESIZE=10000
 # term color
 export TERM=xterm-256color
 
+# ps1
+GIT_PS1_SHOWDIRTYSTATE=1
+GIT_PS1_SHOWUPSTREAM=1
+GIT_PS1_SHOWUNTRACKEDFILES=1
+GIT_PS1_SHOWSTASHSTATE=1
+
+PS1='\[\e]0;\w\a\]\n\[\e[32m\]\u@\h \[\e[33m\]\w\[\e[0m\] <\j>$(__git_ps1)\[\033[00m\]\n\$ '
