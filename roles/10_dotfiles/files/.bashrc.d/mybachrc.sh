94d8c7d266a864308b260c71ae157343875a89f4
Kenichi HIROSE <seroron@outlook.com>
2018-03-29 01:29:52 +0000

    color settings

--- roles/10_dotfiles/files/.bashrc.d/mybachrc.sh
+++ roles/10_dotfiles/files/.bashrc.d/mybachrc.sh
@@ -28,7 +28,15 @@ if [ "$(uname)" == 'Darwin' ]; then
     
 else
     # linux
-    alias ls='ls -F'
+    if [ -x /usr/bin/dircolors ]; then
+        test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+        alias ls='ls -F --color=auto'
+        alias grep='grep --color=auto'
+        alias fgrep='fgrep --color=auto'
+        alias egrep='egrep --color=auto'
+    else
+        alias ls='ls -F'
+    fi
 fi
 
 export PATH=$HOME/.bin:$PATH
