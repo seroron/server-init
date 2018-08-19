e15ac7fe8a5233fa3245dd76727d60ef53e7b7c6
Kenichi HIROSE <seroron@outlook.com>
2018-08-20 07:54:03 +0900

    emacs settings

--- roles/20_basic_packages/setup.sh
+++ roles/20_basic_packages/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if type apt >/dev/null 2>&1; then
-  sudo apt install -y build-essential libncurses5-dev libgnutls-dev
+  sudo -E apt install -y build-essential libncurses5-dev libgnutls-dev
 
 elif type yum >/dev/null 2>&1; then
-  sudo yum groupinstall -y "Development Tools"
-  sudo yum install -y ncurses-devel
+  sudo -E yum groupinstall -y "Development Tools"
+  sudo -E yum install -y ncurses-devel wget gnutls-devel
 fi
 
--- roles/50_emacs/setup.sh
+++ roles/50_emacs/setup.sh
@@ -3,5 +3,5 @@
 git clone https://github.com/seroron/.emacs.d.git ~/.emacs.d
 cd ~/.emacs.d/
 make emacs-local
+. ~/.bashrc.d/emacs-local.sh
 make emacs-package
-
