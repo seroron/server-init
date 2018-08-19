98fca53056eefd268f86cf8e4add23898f2e2343
Kenichi HIROSE <seroron@outlook.com>
2018-08-19 15:03:39 +0900

    basic_packages

--- roles/20_basic_packages/setup.sh
+++ roles/20_basic_packages/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if type apt >/dev/null 2>&1; then
-  sudo apt install -y build-essential libncurses5-dev
+  sudo apt install -y build-essential libncurses5-dev libgnutls-dev
 
 elif type yum >/dev/null 2>&1; then
   sudo yum groupinstall -y "Development Tools"
