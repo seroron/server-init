c6311ba40d509840a5fee1fd511ddc86f5d9f925
Kenichi HIROSE <seroron@outlook.com>
2018-09-29 22:47:49 +0900

    add xml packages

--- roles/20_basic_packages/setup.sh
+++ roles/20_basic_packages/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if type apt >/dev/null 2>&1; then
-  sudo -E apt install -y build-essential libncurses5-dev libgnutls-dev
+  sudo -E apt install -y build-essential libncurses5-dev libgnutls-dev libxml2-dev
 
 elif type yum >/dev/null 2>&1; then
   sudo -E yum groupinstall -y "Development Tools"
-  sudo -E yum install -y ncurses-devel wget gnutls-devel
+  sudo -E yum install -y ncurses-devel wget gnutls-devel libxml2-devel
 fi
 
