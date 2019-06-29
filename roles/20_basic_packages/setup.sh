23dcf5023a2332f3033144fef5a03f8b7072b76c
Kenichi HIROSE <seroron@outlook.com>
2019-06-29 08:00:09 +0000

    ubuntu packages

--- roles/20_basic_packages/setup.sh
+++ roles/20_basic_packages/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if type apt >/dev/null 2>&1; then
-  sudo -E apt install -y build-essential libncurses5-dev libgnutls-dev libxml2-dev libssl-dev libreadline-dev
+  sudo -E apt install -y build-essential libncurses5-dev libgnutls28-dev libxml2-dev libssl-dev libreadline-dev
 
 elif type yum >/dev/null 2>&1; then
   sudo -E yum groupinstall -y "Development Tools"
